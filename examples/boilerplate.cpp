740665e7dd41933a6c248cacb30a0dd243e00d4b
Ted Campbell <tcampbell@mozilla.com>
2019-03-10 20:03:26 -0400

    examples: Add boilerplate code file to use in examples
    This encapsulates the minimal context and global initialization code
    used by most examples. It should be kept and straight-forward as
    possible as it is expected to be read and understood by embedders.
    
    The definition of DefaultGlobalClassOps matches that of SM 65+. It uses
    standard resolve hooks for the builtin standard types.

---
+++ examples/boilerplate.cpp
@@ -0,0 +1,66 @@
+#include <jsapi.h>
+
+#include <js/Initialization.h>
+
+#include "boilerplate.h"
+
+// This file contains boilerplate code used by a number of examples. Ideally
+// this should eventually become part of SpiderMonkey itself.
+
+// A standard set of ClassOps for globals. This includes hooks to resolve
+// standard JavaScript builtin types to give a more full-featured shell.
+const JSClassOps boilerplate::DefaultGlobalClassOps = {
+    nullptr,                         // addProperty
+    nullptr,                         // deleteProperty
+    nullptr,                         // enumerate
+    JS_NewEnumerateStandardClasses,  // newEnumerate
+    JS_ResolveStandardClass,         // resolve
+    JS_MayResolveStandardClass,      // mayResolve
+    nullptr,                         // finalize
+    nullptr,                         // call
+    nullptr,                         // hasInstance
+    nullptr,                         // construct
+    JS_GlobalObjectTraceHook         // trace
+};
+
+// Create a simple Global object. A global object is the top-level 'this' value
+// in a script and is required in order to compile or execute JavaScript.
+JSObject* boilerplate::CreateGlobal(JSContext* cx) {
+  JS::CompartmentOptions options;
+
+  static JSClass BoilerplateGlobalClass = {"BoilerplateGlobal",
+                                           JSCLASS_GLOBAL_FLAGS,
+                                           &boilerplate::DefaultGlobalClassOps};
+
+  return JS_NewGlobalObject(cx, &BoilerplateGlobalClass, nullptr,
+                            JS::FireOnNewGlobalHook, options);
+}
+
+// Initialize the JS environment, create a JSContext and run the example
+// function in that context. By default the self-hosting environment is
+// initialized as it is needed to run any JavaScript). If the 'initSelfHosting'
+// argument is false, we will not initialize self-hosting and instead leave
+// that to the caller.
+bool boilerplate::RunExample(bool (*task)(JSContext*), bool initSelfHosting) {
+  if (!JS_Init()) {
+    return false;
+  }
+
+  JSContext* cx = JS_NewContext(JS::DefaultHeapMaxBytes);
+  if (!cx) {
+    return false;
+  }
+
+  if (initSelfHosting && !JS::InitSelfHostedCode(cx)) {
+    return false;
+  }
+
+  if (!task(cx)) {
+    return false;
+  }
+
+  JS_DestroyContext(cx);
+  JS_ShutDown();
+
+  return true;
+}
