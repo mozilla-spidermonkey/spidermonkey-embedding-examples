c8f45ad64f1366b4b30d89cb650d04e0f7eb8d2a
sagu <16504129+sagudev@users.noreply.github.com>
2021-06-17 19:25:25 +0200

    Update generic_lib.sh

--- tools/generic_lib.sh
+++ tools/generic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Make SpiderMonkey and this repo generic instead of version specific.
-# script shoud be run in mozjs dit with $1 for mason.build location
+# script shoud be run in mozjs dir with $1 for mason.build location
 sed -i 's/mozjs-$MOZILLA_SYMBOLVERSION/mozjs/g' old-configure.in
 sed -i --regexp-extended "s/dependency\(.mozjs.*$/dependency('mozjs')/gm" "$1"
